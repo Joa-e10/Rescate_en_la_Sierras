7bec9f9c39a759aadb20e36a5517e3a9e816097b
javierblengini <javierignacioblengini@upc.edu.ar>
2025-10-12 10:09:39 -0300

    ajustes en ui

---
+++ Assets/Scripts/LifePlayerUi.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifePlayerUi : MonoBehaviour
+{
+    public Image barraVidaPlayer;
+    private PlayerController playerController;
+    private float vidaMaxima;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
